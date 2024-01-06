0c3d9494a1969d844c5e0151bc9af1e28ac9d712
Orcoholist <orcoholist@mail.ru>
2024-01-06 19:02:43 +0300

    sec

--- notes-app-server/src/index.ts
+++ notes-app-server/src/index.ts
@@ -8,11 +8,69 @@ const prisma = new PrismaClient();
 app.use(express.json());
 app.use(cors());
 
-app.get("/api/notes", async(req, res) => {
-    const notes = await prisma.note.findMany();
-    res.json(notes);
+app.get("/api/notes", async (req, res) => {
+  const notes = await prisma.note.findMany();
+  res.json(notes);
+});
+
+app.post("/api/notes", async (req, res) => {
+  const { title, content } = req.body;
+  if (!title || !content) {
+    return res.status(400).send("Title and content are required");
+  }
+  try {
+    const note = await prisma.note.create({
+      data: {
+        title,
+        content,
+      },
+    });
+    res.json(note);
+  } catch (error) {
+    res.status(500).send("Failed to create note");
+  }
+});
+
+app.put("/api/notes/:id", async (req, res) => {
+  const { title, content } = req.body;
+  const  id  = parseInt(req.params.id);
+
+  if (!id || isNaN(id)) {
+    return res.status(400).json({ error: "Invalid note ID" });
+  }
+  try {
+    const updateNote = await prisma.note.update({
+      where: {
+        id,
+      },
+      data: {
+        title,
+        content,
+      },
+    });
+    res.json(updateNote);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to update note" });
+  }
+});
+
+app.delete("/api/notes/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  if (!id || isNaN(id)) {
+    return res.status(400).json({ error: "Invalid note ID" });
+  }
+  try {
+    const deletedNote = await prisma.note.delete({
+      where: {
+        id,
+      },
+    });
+    res.json(deletedNote);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to delete note" });
+  }
 })
 
 app.listen(5000, () => {
-    console.log("Server is running on port 5000");
-})
+  console.log("Server is running on port 5000");
+});
